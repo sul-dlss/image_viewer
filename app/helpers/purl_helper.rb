70e25eacf55052a102260a5fd18c5fbab404d112
Chris Beer <cabeer@stanford.edu>
2015-10-20 10:55:50 -0700

    Extract #purl_url helper instead of passing around partial locals

--- app/helpers/purl_helper.rb
+++ app/helpers/purl_helper.rb
@@ -5,4 +5,10 @@ module PurlHelper
   def get_image_json_array
     Purl::Util.get_image_json_array(@purl)
   end
+
+  def purl_url(druid = nil)
+    druid ||= @purl.druid
+
+    Settings.purl.url + '/' + @purl.druid
+  end
 end
